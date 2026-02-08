c9e4d7c5c76e995a70a841588b1eee658445fc42
coslne <coslne@outlook.com>
2026-02-08 20:41:11 +0800

    Force add all files including ignored ones

---
+++ managed_components/78__esp-wifi-connect/include/ssid_manager.h
@@ -0,0 +1,36 @@
+#ifndef SSID_MANAGER_H
+#define SSID_MANAGER_H
+
+#include <string>
+#include <vector>
+
+struct SsidItem {
+    std::string ssid;
+    std::string password;
+    std::string username;
+};
+
+class SsidManager {
+public:
+    static SsidManager& GetInstance() {
+        static SsidManager instance;
+        return instance;
+    }
+
+    void AddSsid(const std::string& ssid, const std::string& password, const std::string& username = "");
+    void RemoveSsid(int index);
+    void SetDefaultSsid(int index);
+    void Clear();
+    const std::vector<SsidItem>& GetSsidList() const { return ssid_list_; }
+
+private:
+    SsidManager();
+    ~SsidManager();
+
+    void LoadFromNvs();
+    void SaveToNvs();
+
+    std::vector<SsidItem> ssid_list_;
+};
+
+#endif // SSID_MANAGER_H
